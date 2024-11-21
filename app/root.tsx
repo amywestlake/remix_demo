d91369a5cfd23d3eb65fe775c538685438479510
Amy Westlake <amy.westlake@bfi.org.uk>
2024-11-21 12:44:48 +0000

    fix character encoding

--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,7 @@ export default function App() {
   return (
     <html>
       <head>
+      <meta charSet="utf-8" />
       <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
         <link
           rel="icon"
